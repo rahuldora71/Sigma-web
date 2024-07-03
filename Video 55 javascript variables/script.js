6a7af570352b2607d0e7203a0f56729422c6e7b2
rahuldora71 <rahuldoraksp02@gmail.com>
2024-07-03 22:59:16 +0530

    netflix clone and js starts

---
+++ Video 55 javascript variables/script.js
@@ -0,0 +1,11 @@
+console.log("Hi Rahul Dora")
+
+var a=5;
+var b=6;
+var c="Rahul Droa";
+console.log(c+a*(b+2))
+console.log(typeof a, typeof b, typeof c)
+
+
+const a1=6;
+// we cant change the value of const variable
---
+++ Video 56 JS Conditionals/script.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+
+let a=3;
+
+if(a>30){
+    console.log("The value of a is greater than 30");
+}
+else{
+    console.log("The value of a is less than 30");
+}
+
+
